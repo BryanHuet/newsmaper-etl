e632176ae15b3eade3ce20b0c5423fbb4259afbe
BryanHuet <huetb050@gmail.com>
2024-01-24 17:56:55 +0100

    debug duplicates in load

--- dags/Operations.py
+++ dags/Operations.py
@@ -10,6 +10,11 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.decorators import task
 
+
+def is_in_base(row, base, columns):
+    return (row[columns] == base[columns]).all(axis=1).any()
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -38,17 +43,20 @@ with DAG(
         renamed = data.rename(columns={'author':'source', 'url':'link', 'publishedAt':'origin'})
         renamed['country_id'] = 1
         renamed['timestamp'] = datetime.now().isoformat()
+        logging.info(renamed.head(5)[['source', 'title']])
         return renamed
     
     @task
     def load(data):
         engine = sqlalchemy.create_engine('postgresql://airflow:airflow@host.docker.internal:5432/airflow')
         with engine.connect() as conn:
-                result = conn.execute(text("SELECT * FROM news")).fetchall()
+            result = conn.execute(text("SELECT * FROM news")).fetchall()
+        
         database = pd.DataFrame(result)
-        data['is_in'] = data[['source','title','link','origin']].isin(database).apply(sum,1) == 4
-        data = data.loc[data['is_in']==False]
-        data = data[['source', 'title', 'link', 'origin', 'timestamp', 'country_id']]
+        columns = ['source', 'title', 'link', 'origin']
+        row_exists = data.apply(is_in_base, base=database, columns=columns, axis=1)
+        data = data[~row_exists]
+
         logging.info(f'load {len(data)} new rows.')
         data.to_sql(name='news', con=engine, if_exists='append', index=False)
     load(transform(extract()))
