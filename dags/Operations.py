2d82bf966bfd0938d8393308fc3deef057b96bbe
BryanHuet <huetb050@gmail.com>
2024-02-06 15:04:08 +0100

    add new @task drop duplicate

--- dags/Operations.py
+++ dags/Operations.py
@@ -1,5 +1,6 @@
 import airflow
 import pandas as pd
+import numpy as np
 import requests
 import logging
 import json
@@ -14,6 +15,7 @@ from airflow.decorators import task
 def is_in_base(row, base, columns):
     return (row[columns] == base[columns]).all(axis=1).any()
 
+columns = ['source', 'title', 'country_id', 'link', 'timestamp', 'origin']     
 
 default_args = {
     'owner': 'airflow',
@@ -29,6 +31,24 @@ with DAG(
     tags=["dev"]
 ) as dag:
     
+    @task
+    def drop_duplicate():
+        engine = sqlalchemy.create_engine('postgresql://airflow:airflow@host.docker.internal:5432/airflow')
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM news")).fetchall()   
+        database = pd.DataFrame(result, index=np.array(result)[:,0])[columns]
+        if len(database) == 0:
+            return
+        no_duplicates = database.drop_duplicates()
+        if len(no_duplicates) == len(database):
+            return
+        duplicates = database.loc[database.index.drop(no_duplicates.index)]
+        with engine.connect() as conn:
+            for i in range(0, len(duplicates)):
+                row = duplicates.iloc[i]
+                sql = f"DELETE FROM news WHERE id={row.name}"
+                conn.execute(text(sql))
+
     @task
     def extract():
         url = 'https://newsapi.org/v2/top-headlines?country=fr&apiKey=6a0ba42d4da24db8ba63dd4f04460798'
@@ -59,4 +79,4 @@ with DAG(
 
         logging.info(f'load {len(data)} new rows.')
         data.to_sql(name='news', con=engine, if_exists='append', index=False)
-    load(transform(extract()))
+    drop_duplicate() >> load(transform(extract()))
